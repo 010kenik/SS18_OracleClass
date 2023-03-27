1a031cdef4f64a55808b082609975d80fc7fc688
kenik T495 <o1091253653@gmail.com>
2023-03-27 17:09:47 +0900

    제목 : MERGE, CONSTRAINT,JOIN

---
+++ days12/Localhost_HR_Days12_20230327.sql
@@ -0,0 +1,97 @@
+-- HR --
+
+----------------------------------------------------------------------------------------------------------------------------------
+-- 피봇 마지막 문제 --
+1. 테이블 생성 : TBL_PIVOT
+2.       컬럼  : no, name , jumsu     국어, 영어, 수학
+                            kor , eng, mat
+-- 테이블 설계가 잘못되었다.
+
+-- 정규화 잘됨. ( 제 1 정규화 위배 ) DB 모델링
+--1    홍길동  90
+--2    홍길동  89
+--3    홍길동  99
+--
+--4    홍길동  90  89   99
+
+CREATE TABLE TBL_PIVOT
+(
+    no NUMBER NOT NULL  PRIMARY KEY -- 고유한키 PK
+    , name VARCHAR2(20) NOT NULL
+    , jumsu NUMBER(3)  -- NULL 허용
+)
+-- Table TBL_PIVOT이(가) 생성되었습니다.
+
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 1, '박예린', 90 );  -- kor
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 2, '박예린', 89 );  -- eng
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 3, '박예린', 99 );  -- mat
+ 
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 4, '안시은', 56 );  -- kor
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 5, '안시은', 45 );  -- eng
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 6, '안시은', 12 );  -- mat 
+ 
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 7, '김민', 99 );  -- kor
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 8, '김민', 85 );  -- eng
+INSERT INTO TBL_PIVOT ( no, name, jumsu ) VALUES ( 9, '김민', 100 );  -- mat 
+
+COMMIT; 
+
+SELECT * 
+FROM tbl_pivot;
+ 
+1	박예린	90  k   1
+2	박예린	89  e   2
+3	박예린	99  m   0
+4	안시은	56     1
+5	안시은	45    2
+6	안시은	12
+7	김민	99
+8	김민	85
+9	김민	100
+ 
+ -- 질문) 피봇
+번호 이름 국,영,수
+1 박예린  90 89 99
+2 안시은  56 45 12
+3 김민    99 85 100
+-- 풀이.
+  1) 피봇대상 *** 
+-- 12:01 수업 시작~   
+  IN ( 국어, 영어, 수학  )
+  
+  SELECT *
+  FROM (
+          SELECT   TRUNC( (no-1)/3 )  + 1  no
+                , name
+                , jumsu
+                , DECODE( MOD( no, 3), 1, '국어', 2, '영어', 0, '수학' ) subject -- 과목
+          FROM tbl_pivot
+  )
+  PIVOT(  MAX(jumsu)  FOR subject   IN ('국어',  '영어',  '수학'))
+  ORDER BY no ASC;  
+1월     12월 
+
+-- 조민경
+SELECT *
+FROM (
+    SELECT name, jumsu
+    , ROW_NUMBER() OVER(PARTITION BY name ORDER BY no) r -- subejct
+    FROM tbl_pivot
+    )
+PIVOT( SUM(jumsu)   FOR  r  IN ( 1 AS "국", 2"영", 3"수"));
+
+-- 자바 - 임의의 수(난수)     0.0 <=   Math.radnom()   < 1.0
+-- Oracle :  dbms_random 패키지 == 관련 함수, 프로시저 등등
+--           PL/SQL  6가지 종류 : 패키지(package)
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+               
+                
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ days12/Localhost_SYS_Days12_20230327.sql
@@ -0,0 +1,173 @@
+--  SYS --
+
+
+
+
+
+
+
+
+ -- FIRST / LAST 함수
+SELECT MIN(sal) KEEP ( DENSE_RANK FIRST ORDER BY sal DESC )  "min_pay"
+    , MAX(sal) KEEP ( DENSE_RANK LAST ORDER BY sal )  "max_pay"
+    , SUM(sal) KEEP ( DENSE_RANK FIRST ORDER BY sal )  "min_pay"
+    , AVG(sal) KEEP ( DENSE_RANK LAST ORDER BY sal )  "max_pay"
+FROM emp;
+--------------------------------------------------------------------------------------------------------------------------------
+-- 조민경 --
+SELECT d.deptno
+    , COUNT(e.deptno) 부서원수
+FROM emp e RIGHT OUTER JOIN dept d ON e.deptno = d.deptno 
+GROUP BY d.deptno
+ORDER BY d.deptno ASC;
+--
+SELECT b.buseo, a.jikwi, nvl(b.cnt,0)
+FROM (SELECT DISTINCT jikwi from INSA) a left join (SELECT buseo, jikwi, COUNT(jikwi) cnt 
+                                                    FROM insa
+                                                    GROUP BY buseo, jikwi) b 
+PARTITION BY (b.buseo) 
+on a.jikwi = b.jikwi                                
+order by buseo, jikwi;
+
+--참고사이트 :   
+--https://blog.naver.com/pino93/222708884741
+--https://blog.naver.com/blacksmail/222349045230
+--https://jungmina.com/858
+
+ORACLE 10g  PARTITION OUTER JOIN 구문 ㅠㅠ
+--------------------------------------------------------------------------------------------------------------------------------
+[ 문제 ]
+SELECT year, m.month,  NVL( COUNT( empno ), 0) n
+FROM 
+   ( SELECT  empno, TO_CHAR(hiredate,'YYYY') year, TO_NUMBER(TO_CHAR(hiredate,'MM')) month  FROM emp ) e 
+PARTITION BY ( e.year  )   
+RIGHT JOIN 
+   (SELECT LEVEL month FROM dual CONNECT BY LEVEL <= 12) m    
+ON e.month = m.month
+GROUP BY year, m.month
+ORDER BY year, m.month;
+--
+SELECT LEVEL  FROM dual CONNECT BY LEVEL <= 12;
+
+YEAR      MONTH          N
+---- ---------- ----------
+1980          1          0
+1980          2          0
+1980          3          0
+1980          4          0
+1980          5          0
+1980          6          0
+1980          7          0
+1980          8          0
+1980          9          0
+1980         10          0
+1980         11          0
+
+YEAR      MONTH          N
+---- ---------- ----------
+1980         12          1
+1981          1          0
+1981          2          2
+1981          3          0
+1981          4          1
+1981          5          1
+1981          6          1
+1981          7          0
+1981          8          0
+1981          9          2
+1981         10          0
+
+YEAR      MONTH          N
+---- ---------- ----------
+1981         11          1
+1981         12          2
+1982          1          1
+1982          2          0
+1982          3          0
+1982          4          0
+1982          5          0
+1982          6          0
+1982          7          0
+1982          8          0
+1982          9          0
+
+YEAR      MONTH          N
+---- ---------- ----------
+1982         10          0
+1982         11          0
+1982         12          0
+
+36개 행이 선택되었습니다. 
+--------------------------------------------------------------------------------------------------------------------------------
+[문제]
+SELECT FLOOR(DBMS_RANDOM.VALUE(1,46))
+FROM dual
+CONNECT BY LEVEL <=6;
+
+[문제]
+SELECT deptno
+     , LISTAGG( ename , '/') WITHIN GROUP ( ORDER BY ename )  as enames    
+FROM emp
+GROUP BY deptno;
+10	CLARK/MILLER
+20	FORD/JONES/SMITH
+30	ALLEN/BLAKE/JAMES/MARTIN/TURNER/WARD
+	KING
+--------------------------------------------------------------------------------------------------------------------------------
+[문제] insa에서 부서별, 직위별, 사원수 
+SELECT buseo, jikwi, COUNT( num ) tot_count
+FROM insa
+GROUP BY buseo, jikwi
+ORDER BY buseo, jikwi;
+-- 부서별 직위별 최소사원수, 최대사원수 파악
+WITH 
+  t1 AS (
+     SELECT buseo, jikwi, COUNT( num ) tot_count
+     FROM insa
+     GROUP BY buseo, jikwi
+  ),
+  t2 AS (
+     SELECT buseo, MIN( tot_count ) buse_min_count,  MAX( tot_count ) buse_max_count
+     FROM t1
+     GROUP BY buseo 
+  )
+SELECT a.buseo
+ , b.jikwi 최소부서, b.tot_count 최소사원수
+ , c.jikwi 최대부서, c.tot_count 최대사원수
+FROM t2 a, t1 b, t1 c
+WHERE a.buseo = b.buseo AND a.buse_min_count = b.tot_count
+    AND a.buseo = c.buseo AND a.buse_max_count = c.tot_count
+ORDER BY 1, 2   ; 
+개발부	부장	1	사원	9
+기획부	부장	2	대리	3
+기획부	사원	2	대리	3
+영업부	과장	1	사원	8
+인사부	과장	1	사원	2
+인사부	대리	1	사원	2
+자재부	과장	1	사원	4
+자재부	부장	1	사원	4
+총무부	부장	1	사원	4
+홍보부	과장	1	사원	3
+-- FIRST / LAST 분석함수
+WITH 
+  temp AS (
+     SELECT buseo, jikwi, COUNT( num ) tot_count
+     FROM insa
+     GROUP BY buseo, jikwi
+  )
+SELECT t.buseo
+   ,MIN(t.jikwi) KEEP ( DENSE_RANK FIRST ORDER BY t.tot_count ) 최소부서 
+   , MIN(t.tot_count) 최소사원수
+   ,MAX(t.jikwi) KEEP ( DENSE_RANK LAST ORDER BY t.tot_count ) 최대부서
+   , MAX(t.tot_count) 최대사원수
+FROM temp t
+GROUP BY t.buseo
+ORDER BY 1, 2;
+개발부	부장	1	사원	9
+기획부	부장	2	대리	3
+영업부	과장	1	사원	8
+인사부	과장	1	사원	2
+자재부	과장	1	사원	4
+총무부	부장	1	사원	4
+홍보부	과장	1	사원	3
+--------------------------------------------------------------------------------------------------------------------------------
